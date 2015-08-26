9517ce0dc67efef6e4fcbc7239107dbc4b5c774d
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-08-26 16:30:58 -0600

    WIP creating post route

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.data._
@@ -8,18 +9,38 @@ import play.api.data.Forms._
 
 object Application extends Controller {
 
-  case class Place(id: Int, name: String)
   case class ShortUrl(id: Int, name: String, longUrl: String)
 
-  implicit val placesWrites = Json.writes[Place]
+  implicit val urlWrites = Json.writes[ShortUrl]
+
+  val form = Form(
+    tuple(
+      "Url" -> text,
+      "Name" -> text
+    )
+  )
+
+  var urls = Array[ShortUrl]()
+  var idCounter = 1
 
   def shortenedURL = Action {
-    var x:Place = new Place(1, "Laguna")
+    var x:ShortUrl = new ShortUrl(1, "http://www.google.com", "Google")
     val json = Json.toJson(x)
     Ok(json)
   }
 
   def index = Action {
+    Ok(views.html.main())
+  }
+
+  def create = Action { implicit request =>
+    val params = form.bindFromRequest.get
+    var x:ShortUrl = new ShortUrl(idCounter, params._1, params._2)
+    urls = urls :+ x
+    idCounter = idCounter + 1
+    println(x.name)
+    println(x.longUrl)
+    println(urls)
     Ok(views.html.main())
   }
 
