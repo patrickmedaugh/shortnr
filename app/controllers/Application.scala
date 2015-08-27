6c2484c64fee74e0f4c6d6bfa62971828f628fac
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-08-27 14:14:46 -0600

    Basic URL validation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,17 +15,19 @@ object Application extends Controller {
 
   val form = Form(
     tuple(
-      "Url" -> text,
-      "Name" -> text
+      "Name" -> text,
+      "Url" -> text
     )
   )
 
   var urls = Array[ShortUrl]()
+
   var idCounter = 1
 
+  val validUrl ="^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".r;
+
   def shortenedURL = Action {
-    var x:ShortUrl = new ShortUrl(1, "http://www.google.com", "Google")
-    val json = Json.toJson(x)
+    val json = Json.toJson(urls)
     Ok(json)
   }
 
@@ -35,12 +37,14 @@ object Application extends Controller {
 
   def create = Action { implicit request =>
     val params = form.bindFromRequest.get
-    var x:ShortUrl = new ShortUrl(idCounter, params._1, params._2)
+    val url = params._2 match {
+      case validUrl(_*) => params._2
+      case _ => ""
+    }
+    var x:ShortUrl = new ShortUrl(idCounter, params._1, url)
     urls = urls :+ x
     idCounter = idCounter + 1
-    println(x.name)
-    println(x.longUrl)
-    println(urls)
+    println(urls(urls.length - 1))
     Ok(views.html.main())
   }
 
